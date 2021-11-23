e0dd7b6fafe29364c9768b9244191d9fc3e79e46
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-23 14:05:38 +0000

    handled exception in all controller functions

--- src/controllers/books_controller.js
+++ src/controllers/books_controller.js
@@ -1,18 +1,26 @@
 const {bookService} = require('../services');
 
 const getBooks = async (req, res) => {
-  const books = await bookService.getBooks();
-  res.json(books).status(200);
+  try {
+    const books = await bookService.getBooks();
+    res.json(books).status(200);
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
 };
 
 const getBook = async (req, res) => {
-  const bookId = req.params.bookId;
-  const book = await bookService.getBook(Number(bookId));
+  try {
+    const bookId = req.params.bookId;
+    const book = await bookService.getBook(Number(bookId));
 
-  if (book) {
-    res.json(book).status(200);
-  } else {
-    res.status(404).json('Not found');
+    if (book) {
+      res.json(book).status(200);
+    } else {
+      res.status(404).json('Not found');
+    }
+  } catch (error) {
+    res.status(400).json({message: error.message});
   }
 };
 
@@ -28,10 +36,14 @@ const saveBook = async (req, res) => {
 
 // User Story 4 - Update Book By Id Solution
 const updateBook = async (req, res) => {
-  const bookUpdateData = req.body;
-  const bookId = req.params.bookId;
-  const book = await bookService.updateBook(bookId, bookUpdateData);
-  res.status(204).json(book);
+  try {
+    const bookUpdateData = req.body;
+    const bookId = req.params.bookId;
+    const book = await bookService.updateBook(bookId, bookUpdateData);
+    res.status(204).json(book);
+  } catch (error) {
+    res.status(400).json({message: error.message});
+  }
 };
 
 const deleteBook = async (req, res) => {
