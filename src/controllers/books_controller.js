a8e9a5baf82dd326b469052b0a7e031fdb6856e5
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-23 13:58:11 +0000

    add try catch for delete function

--- src/controllers/books_controller.js
+++ src/controllers/books_controller.js
@@ -36,12 +36,15 @@ const updateBook = async (req, res) => {
 
 const deleteBook = async (req, res) => {
   const bookId = req.params.bookId;
-  const book = await bookService.deleteBook(Number(bookId));
-
-  if (book) {
-    res.json(book).status(200);
-  } else {
-    res.status(404).json('Not found');
+  try {
+    const book = await bookService.deleteBook(Number(bookId));
+    if (book) {
+      res.json(book).status(200);
+    } else {
+      res.status(404).json('Not found');
+    }
+  } catch (error) {
+    res.status(400).json({message: error.message});
   }
 };
 
