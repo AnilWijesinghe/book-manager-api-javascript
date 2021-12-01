cfaa7af96110f1245ec5004914ef264883a7f55c
Anil Wijesinghe <anilanjuna@gmail.com>
2021-12-01 16:37:30 +0000

    swagger setup completely

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 module.exports = Object.freeze({
   NOT_FOUND: `Not found`,
+  RETRIEVE_SUC_ALL: `Books are retrieved successfully`,
+  RETRIEVE_SUC_BOOK: `Book is retrieved successfully`,
+  SAVE_SUC_BOOK: `Book has been saved successfully`,
   BOOK_EXIST: `Book is already exist`,
   BOOK_NOT_FOUND: `Book is not found`,
   DEL_SUCCESS: `Successfully deleted`,
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {booksController} = require('../controllers');
 
 const router = new express.Router();
+
 router.get('/books', booksController.getBooks);
 router.get('/books/:bookId', booksController.getBook);
 router.post('/books', booksController.saveBook);
